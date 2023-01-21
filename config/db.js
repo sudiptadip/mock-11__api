3764dc7b41f4c53693f454436fb6e6e82419d560
sudiptadip <sudiptabhattacharjee000@gmail.com>
2023-01-21 10:23:54 +0530

    add

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
-
-const url = `mongodb+srv://sudiptadip:sudipta12dip@cluster0.cjh7zwa.mongodb.net/?retryWrites=true&w=majority`
+mongoose.set('strictQuery', true)
+const url = `mongodb+srv://sudiptadip:sudipta12dip@cluster0.3yv6bgv.mongodb.net/?retryWrites=true&w=majority`
 
 const connection = mongoose.connect(url)
 
